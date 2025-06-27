2f1de0b48d44d2f38e4661309c9f5c204501db7a
eunju <164160204+eunjooo2@users.noreply.github.com>
2025-06-27 22:16:30 +0900

    코드 정리

--- lib/data/achievement.dart
+++ lib/data/achievement.dart
@@ -8,10 +8,10 @@ class Achievement {
   final String title;
   final String description;
   final String type;
-  final bool isOnce;
-  final bool isHidden;
-  final String? hint;
-  final Map<String, int> amount;
+  final bool isOnce; // 일회성업적
+  final bool isHidden; // 히든업적
+  final String? hint; 
+  final Map<String, int> amount; // 등급 임계값
   final Map<String, dynamic>? reward;
 
   Achievement({
@@ -21,11 +21,13 @@ class Achievement {
     required this.type,
     required this.amount,
     required this.isOnce,
-    this.isHidden = false,
-    this.reward,
+    this.isHidden = false, // 
+    this.reward, 
     this.hint,
   });
 
+/// 일반 JSON 데이터를 받아 Achievement 객체로 변환하는 팩토리 생성자
+/// 서버 응답 또는 로컬 파일에서 불러온 JSON 구조를 처리
   factory Achievement.fromJson(Map<String, dynamic> json) {
     return Achievement(
       id: json["id"] ?? '',
@@ -33,14 +35,7 @@ class Achievement {
       description: json['description'] ?? '',
       type: json['type'] ?? '',
       isOnce: json['isOnce'],
-      isHidden: json['isHidden'] ?? false,
-      amount: Map<String, int>.from(json['amount'] ?? {}),
-      reward: json['reward'],
-      hint: json['hint'] ?? '', // null 막기
-    );
-  }
-
-  Map<String, dynamic> toJson() {
+      isHidden: json['isHidden']
     return {
       'title': title,
       'description': description,
@@ -52,14 +47,16 @@ class Achievement {
     };
   }
 
-  factory Achievement.fromRecord(RecordModel record) {
+/// PocketBase에서 불러온 RecordModel을 기반으로 Achievement 객체를 생성하는 팩토리 생성자
+/// PocketBase의 record.data에서 각 필드를 추출하여 초기화     
+  factory Achievement.fromRecord(RecordModel record) { 
     return Achievement(
       id: record.data["id"],
       title: record.data["title"],
       description: record.data["description"],
       type: record.data["type"],
       amount: Map<String, int>.from(record.data["amount"]),
-      reward: record.data["reward"] as Map<String, dynamic>,
+      reward: record.data["reward"] != null ? Map<String, dynamic>.from(record.data["reward"]) : null,
       isOnce: record.data["isOnce"],
       isHidden: record.data["isHidden"],
       hint: record.data["hint"] ?? '',
